e98d8a7dbc508b11985d74dffebef5cc99dc412f
Rhoxolan <pba17mail@gmail.com>
2024-03-20 23:00:35 +0200

    Some changes

--- Recruiting Company Web API/Services/GuestServices/GuestService/GuestService.cs
+++ Recruiting Company Web API/Services/GuestServices/GuestService/GuestService.cs
@@ -10,37 +10,45 @@ namespace Recruiting_Company_Web_API.Services.GuestServices.GuestService
 		private readonly ApplicationContext _context;
 
 		public GuestService(ApplicationContext context)
-			=> _context = context;
+		{
+			_context = context;
+		}
 
 		public async Task<int> GetVacanciesCountAsync(VacancyRequestParameters requestParameters)
-			=> await GetVacancies(requestParameters).CountAsync();
+		{
+			return await GetVacancies(requestParameters).CountAsync();
+		}
 
 		public async Task<IEnumerable<dynamic>> GetVacanciesAsync(VacancyRequestParameters requestParameters)
-			=> await GetVacancies(requestParameters)
-				.Skip((requestParameters.PageNumber - 1) * requestParameters.Pagesize)
-				.Take(requestParameters.Pagesize)
-				.Select(v => new
-				{
-					v.Id,
-					CategoryID = v.Category.Id,
-					EmployerID = v.Employer.PublicId,
-					Employer = v.Employer.CompanyName,
-					v.CreateDate,
-					v.Title,
-					v.Location,
-					v.Salary,
-					v.PhoneNumber,
-					v.EMail,
-					v.Description
-				})
-				.ToListAsync();
+		{
+			return await GetVacancies(requestParameters)
+						.Skip((requestParameters.PageNumber - 1) * requestParameters.Pagesize)
+						.Take(requestParameters.Pagesize)
+						.OrderBy(v => v.CreateDate)
+						.Select(v => new
+						{
+							v.Id,
+							CategoryID = v.Category.Id,
+							EmployerID = v.Employer.PublicId,
+							Employer = v.Employer.CompanyName,
+							v.CreateDate,
+							v.Title,
+							v.Location,
+							v.Salary,
+							v.PhoneNumber,
+							v.EMail,
+							v.Description
+						})
+						.ToListAsync();
+		}
 
 		private IQueryable<Vacancy> GetVacancies(VacancyRequestParameters requestParameters)
-			=> _context.Vacancies
-				.Include(v => v.Category)
-				.Include(v => v.Employer)
-				.Where(v => requestParameters.CategoryID == null || v.Category.Id == requestParameters.CategoryID)
-				.Where(v => requestParameters.EmployerID == null || v.Employer.PublicId.ToString() == requestParameters.EmployerID);
-
+		{
+			return _context.Vacancies
+						.Include(v => v.Category)
+						.Include(v => v.Employer)
+						.Where(v => requestParameters.CategoryID == null || v.Category.Id == requestParameters.CategoryID)
+						.Where(v => requestParameters.EmployerID == null || v.Employer.PublicId.ToString() == requestParameters.EmployerID);
+		}
 	}
 }
