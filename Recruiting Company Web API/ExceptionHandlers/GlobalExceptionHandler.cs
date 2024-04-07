4768aa5c46b605956be69b12ee9c7a488ea35c64
Rhoxolan <pba17mail@gmail.com>
2024-04-08 00:31:06 +0300

    Modified the exception handler

--- Recruiting Company Web API/ExceptionHandlers/GlobalExceptionHandler.cs
+++ Recruiting Company Web API/ExceptionHandlers/GlobalExceptionHandler.cs
@@ -3,19 +3,26 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Recruiting_Company_Web_API.ExceptionHandlers
 {
-	public class GlobalExceptionHandler : IExceptionHandler
+	public class GlobalExceptionHandler(IProblemDetailsService problemDetailsService) : IExceptionHandler
 	{
 		public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
 		{
 			var problemDetails = new ProblemDetails
 			{
-				Detail = "Error. Please contact to developer",
-				Status = StatusCodes.Status500InternalServerError,
-				Type = "https://tools.ietf.org/html/rfc9110#section-15.6.1",
-				Title = "An error occurred while processing your request."
+				Detail = "Error. Please contact to developer"
 			};
-			httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
-			await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+			var problemDetailsContext = new ProblemDetailsContext
+			{
+				HttpContext = httpContext,
+				ProblemDetails = problemDetails
+			};
+			if (!await problemDetailsService.TryWriteAsync(problemDetailsContext))
+			{
+				problemDetails.Status = StatusCodes.Status500InternalServerError;
+				problemDetails.Title = "An error occurred while processing your request.";
+				httpContext.Response.StatusCode = problemDetails.Status.Value;
+				await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+			}
 			return await ValueTask.FromResult(true);
 		}
 	}
