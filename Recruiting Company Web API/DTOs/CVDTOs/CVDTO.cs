b7fa1ad66f2f4d1b738f4bf1b401783d2dbf9ceb
Rhoxolan <pba17mail@gmail.com>
2024-04-27 23:25:40 +0300

    Added CV DTO

---
+++ Recruiting Company Web API/DTOs/CVDTOs/CVDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace Recruiting_Company_Web_API.DTOs.CVDTOs
+{
+	public class CVDTO
+	{
+		public required ulong? Id { get; set; }
+
+		public required DateTime? UploadDate { get; set; }	
+
+		public required string? File {  get; set; }
+
+		public required string? Format { get; set; }
+
+		public required bool? IsFile { get; set; }
+
+		public required bool? IsLink { get; set; }
+
+		public required string? Link { get; set; }
+	}
+}
--- Recruiting Company Web API/Services/SeekerServices/CVService/ICVService.cs
+++ Recruiting Company Web API/Services/SeekerServices/CVService/ICVService.cs
@@ -1,11 +1,12 @@
-﻿using Recruiting_Company_Web_API.Models.SeekerModels;
+﻿using Recruiting_Company_Web_API.DTOs.CVDTOs;
+using Recruiting_Company_Web_API.Models.SeekerModels;
 
 namespace Recruiting_Company_Web_API.Services.SeekerServices.CVService
 {
 	public interface ICVService
 	{
-		Task<ServiceResult<IEnumerable<dynamic>>> GetCVsAsync(string name);
-		Task<ServiceResult<dynamic>> UploadCVAsync(CVModel model, string name);
+		Task<ServiceResult<List<CVDTO>>> GetCVsAsync(string name);
+		Task<ServiceResult<CVDTO>> UploadCVAsync(CVModel model, string name);
 		Task<ServiceResult> DeleteCVAsync(ulong id, string name);
 	}
 }
